d58a6af1a9878950d113f8d505155db4c0c4ff9c
David Milanes <davidmlnsgross@gmail.com>
2025-03-06 23:13:45 -0500

    Initial application configured (tailwindcss installed)

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function MiComponente() {
+  return (
+    <View className="bg-white p-4">
+      <Text className="text-yellow-500 text-lg">Hola Mundo</Text>
+    </View>
+  );
+}
